37ae32e8d51f93a3c5f89b6a01d6dea7a3a5d82f
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-07-12 11:10:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('Hello!');
+var object = new (function () {
+  this.name = 'Sudheer';
+})();
+console.log(object);
+
+//object literals
+//object create method
+//object constructor new Object()
+//function constructor
+//class instantiation
+const newObj = Object.create({});
+newObj.age = 20;
+console.log(newObj);
+let proto = Object.getPrototypeOf(object);
+console.log(proto);
+let proto2 = Object.getPrototypeOf(proto);
+console.log(proto2);
+let proto3 = Object.getPrototypeOf(proto2);
+console.log(proto3);
+
+const person1 = { fname: 'sree', lname: 'lekha' };
+const person2 = { fname: 'soumya', lname: 'sree' };
+
+function fullName(greeting) {
+  console.log(`${greeting} My Name is ${this.fname} ${this.lname}`);
+}
+// console.log(newObj.__proto__);
+
+fullName.call(person1, 'Hi');
+fullName.call(person2, 'Hello');
+
+fullName.apply(person1, ['Good Mng']);
+
+let myFullName = fullName.bind(person1);
+
+console.log(myFullName('How are you?'));
+
+const numArr = [1, 2, 3, 4, 5, 6];
+const returnAr = numArr.splice(2, 2);
+console.log(numArr);
+console.log(returnAr);
